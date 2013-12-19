df45ed1e3e0ce074014b6edfa37dc150237300cc
khsheehan <kyle@curalate.com>
2013-12-19 16:09:24 -0500

    Added most useful PHP String functions.

--- src/javascripts/phollyfill.js
+++ src/javascripts/phollyfill.js
@@ -1,8 +1,4 @@
 /**
- * phollyfill.js
- */
-
-/**
  * String.contains()
  * @param str           The string to search for
  * @param startIndex    The index to start searching at
@@ -54,4 +50,79 @@ if (typeof String.prototype.rtrim !== 'function') {
     String.prototype.rtrim = function() {
         return this.replace(/\s+$/, '');
     };
+}
+
+/**
+ * String.countChars()
+ */
+if (typeof String.prototype.countChars !== 'function') {
+    String.prototype.countChars = function() {
+        var charCount = {},
+            i;
+        for (i = 0; i < this.length; i++) {
+            charCount[this[i]] = charCount[this[i]] + 1 || 1;
+        }
+        return charCount;
+    };
+}
+
+/**
+ * String.repeat()
+ * @param num   The number of times to repeat the string
+ */ 
+if (typeof String.prototype.repeat !== 'function') {
+    String.prototype.repeat = function(num) {
+        return new Array(isNaN(num)? 1 : ++num).join(this);
+    }
+}
+
+/**
+ * String.shuffle()
+ */
+if (typeof String.prototype.shuffle !== 'function') {
+    String.prototype.shuffle = function() {
+        return this.split('').sort(function() {
+            return 0.5-Math.random()
+        }).join('');        
+    }
+}
+
+/**
+ * String.reverse()
+ */
+if (typeof String.prototype.reverse !== 'function') {
+    String.prototype.reverse = function() {
+        return this.split('').reverse().join('');
+    }
+}
+
+/**
+ * String.tokenize()
+ */
+if (typeof String.prototype.tokenize !== 'function') {
+    String.prototype.tokenize = function() {
+        return this.split('');
+    }
+}
+
+/**
+ * String.ucfirst()
+ */
+if (typeof String.prototype.ucfirst !== 'function') {
+    String.prototype.ucfirst = function() {
+        return this.charAt(0).toUpperCase() + this.substr(1);
+    }
+}
+
+/**
+ * String.ucwords()
+ */
+if (typeof String.prototype.ucwords !== 'function') {
+    String.prototype.ucwords = function() {
+        var ucList = [];
+        this.split(" ").forEach(function(s) {
+            ucList.push(s.ucfirst());
+        });
+        return ucList.join(" ");
+    }
 }
